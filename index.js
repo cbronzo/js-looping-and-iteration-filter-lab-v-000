388c16195495084f69ebfdc86116a7e7bc747f67
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 01:12:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function fuzzyMatch(collection, string) {
 
 function matchName(collection, string) {
 	return collection.filter(function (driver) {
-		return driver.name.toLowerCase() === name.toLowerCase();
+		return driver.string.toLowerCase() === string.toLowerCase();
 	})
 }
 
