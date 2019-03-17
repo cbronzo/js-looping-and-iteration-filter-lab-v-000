ec742a3fb944024697ebcae0d3bc4402b9a36d27
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 01:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 function findMatching (collection, string) {
   return collection.filter( function (driver) {
-    return driver.toLowerCase() === string.toLowerCase() 
+    return driver.toLowerCase() === string.toLowerCase(); 
   })
 }
 
-function fuzzyMatch(drivers, name) {
-	let lengthOfName = name.length;
-	return drivers.filter(function(driverName) {
-		return driverName.slice(0,lengthOfName) === name;
+function fuzzyMatch(collection, string) {
+	let nameLength = name.length;
+	return collection.filter(function (driver) {
+		return driver.slice(0, nameLength) === name;
   })
 }
 
 
 function matchName(collection, string) {
-	return collections.filter(function(driver) {
+	return collection.filter(function (driver) {
 		return driver.name.toLowerCase() === name.toLowerCase();
 	})
 }
